bd306e9a1db2262b98b1f6a3fb94b66012652d6d
HiDoYa <hiroyagojo@gmail.com>
2016-10-10 02:40:53 +0000

    Add color

--- Gravity Test/main.js
+++ Gravity Test/main.js
@@ -1,28 +1,41 @@
 //Testing out how gravity would work in simulation
 
 var GRAVITY = 9.8;
-var dimensions = 10;
+var dimensions = 20;
 var x = 300;
 var y = 200;
-var velocity = 30;
+var velocityY = 30;
+var velocityX = 0;
 
 function setup()
 {
-    createCanvas(500, 1000);
-    frameRate(15);
+    createCanvas(1000, 1000);
+    frameRate(30);
 }
 
 function draw()
 {
-    background(255, 255, 255);
-    fill(color(120, 120, 120));
-    ellipse(x, y, dimensions, dimensions);
-    y -= velocity;
-    velocity -= GRAVITY;
+    background(52, 152, 219);
     
-    if (y > 900 || y === 900)
+    //Ground
+    fill(39, 174, 96);
+    rect(0, 900, width, height)
+    
+    //Projectile
+    fill(255, 255 ,255);
+    ellipse(x, y, dimensions, dimensions);
+    
+    //Y movement
+    y -= velocityY;
+    velocityY -= GRAVITY;
+    
+    //X movement
+    x -= velocityX;
+    
+    //Y bounce
+    if (y > (900 - dimensions) || y === (900 - dimensions))
     {
-        velocity = -velocity - 25;
-        y = 899;
+        velocityY = -velocityY - 25;
+        y = 899 - (dimensions / 2);
     }
 }
